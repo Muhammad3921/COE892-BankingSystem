252583acdcc155941ce1348e2d0bdb36afdf1aab
WilliamJYi <yiwilliam1098@gmail.com>
2024-04-05 01:40:12 -0400

    add home-loggedin-page and branch-home-loggedin-page

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,75 +1,68 @@
-import React from 'react';
-import { Navigate, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Nav/Nav';
-import SidePanel from './components/SidePanel/SidePanel';
-import PortalNav from './components/PortalNav/PortalNav';
-import About from './pages/about/about';
-import Contact from './pages/contact/contact';
-import SignUp from './pages/signUp/signUp';
-import Login from './pages/login/login';
-import LogOut from './pages/logout/logout';
+import React from "react";
+import { Navigate, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Nav/Nav";
+import SidePanel from "./components/SidePanel/SidePanel";
+import PortalNav from "./components/PortalNav/PortalNav";
+import About from "./pages/about/about";
+import Contact from "./pages/contact/contact";
+import SignUp from "./pages/signUp/signUp";
+import Login from "./pages/login/login";
+import LogOut from "./pages/logout/logout";
 //import Profile from './pages/profile/profile';
-import PharmaceuticalPortal from './pages/home/PharmaceuticalPortal';
-import PharmaceuticalPortalLog from './pages/home-loggedin/PharmaceuticalPortalLog';
-import './App.css';
+import PharmaceuticalPortal from "./pages/home/PharmaceuticalPortal";
+import Home from "./pages/home-loggedin/Home";
+import "./App.css";
 // import Scheduler from './pages/scheduler/scheduler';
 // import PrescriptionSubmit from './pages/prescription-submit/prescriptionsubmit';
 // import DrugInteractionChecker from './pages/drugInteractionChecker/drugInteractionChecker';
 // import DrugSupplyTracker from './pages/drugSupplyTrack/drugSupplyTrack';
 // import DrugSideEffectReport from './pages/drugSideEffectReport/drugSideEffectReport';
-import PrivateRoute from './privateroute';
-import UnPrivateRoute from './unprivateroute';
-import LogOutPrivateRoute from './logoutprivateroute';
+import PrivateRoute from "./privateroute";
+import UnPrivateRoute from "./unprivateroute";
+import LogOutPrivateRoute from "./logoutprivateroute";
+import BranchHome from "./pages/home-branch-loggedin/BranchHome";
 
 function isAuth() {
-  return (sessionStorage.getItem("token"));
+  return sessionStorage.getItem("token");
 }
 
-function App() {    
+function App() {
   return (
     <>
-      {
-        (() => {
-            if(isAuth() === null) {
-               
-              return (
-                    <Navbar /> 
-              )
-            }else{
-              return (
-                <PortalNav /> 
-              )
-            }
-        })()  
-      }  
-      
-      {
-        (() => {
-            if(isAuth() !== null) {
-              return (
-                    <SidePanel /> 
-              )
-            } 
-        })()  
-      }  
+      {(() => {
+        if (isAuth() === null) {
+          return <Navbar />;
+        } else {
+          return <PortalNav />;
+        }
+      })()}
+
+      {(() => {
+        if (isAuth() !== null) {
+          return <SidePanel />;
+        }
+      })()}
       <Routes>
-        <Route exact path='/home' element={<PrivateRoute/>}>
-          <Route exact path='/home' element={<PharmaceuticalPortalLog/>} />
+        <Route exact path="/home" element={<PrivateRoute />}>
+          <Route exact path="/home" element={<Home />} />
         </Route>
-        <Route exact path='/' element={<UnPrivateRoute/>}>
-          <Route exact path='/' element={<PharmaceuticalPortal/>} />
+        <Route exact path="/home-branch" element={<PrivateRoute />}>
+          <Route exact path="/home-branch" element={<BranchHome />} />
         </Route>
-        <Route exact path='/about' element={<UnPrivateRoute/>}>
-          <Route exact path='/about' element={<About/>} />
+        <Route exact path="/" element={<UnPrivateRoute />}>
+          <Route exact path="/" element={<PharmaceuticalPortal />} />
         </Route>
-        <Route exact path='/services' element={<UnPrivateRoute/>}>
-          <Route exact path='/services' element={<Contact/>} />
+        <Route exact path="/about" element={<UnPrivateRoute />}>
+          <Route exact path="/about" element={<About />} />
         </Route>
-        <Route exact path='/signup' element={<UnPrivateRoute/>}>
-          <Route exact path='/signup' element={<SignUp/>} />
+        <Route exact path="/services" element={<UnPrivateRoute />}>
+          <Route exact path="/services" element={<Contact />} />
         </Route>
-        <Route exact path='/login' element={<UnPrivateRoute/>}>
-          <Route exact path='/login' element={<Login/>} />
+        <Route exact path="/signup" element={<UnPrivateRoute />}>
+          <Route exact path="/signup" element={<SignUp />} />
+        </Route>
+        <Route exact path="/login" element={<UnPrivateRoute />}>
+          <Route exact path="/login" element={<Login />} />
         </Route>
         {/* <Route exact path='/profile' element={<PrivateRoute/>}>
           <Route exact path='/profile' element={<Profile/>} />
@@ -89,8 +82,8 @@ function App() {
         <Route exact path='/side-effect-report' element={<PrivateRoute/>}>
           <Route exact path='/side-effect-report' element={<DrugSideEffectReport/>} />
         </Route> */}
-        <Route exact path='/logout' element={<LogOutPrivateRoute/>}>
-          <Route exact path='/logout' element={<LogOut/>} />
+        <Route exact path="/logout" element={<LogOutPrivateRoute />}>
+          <Route exact path="/logout" element={<LogOut />} />
         </Route>
       </Routes>
     </>
---
+++ Frontend/src/pages/home-loggedin/Home.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import Paper from "@mui/material/Paper";
+import Grid from "@mui/material/Grid";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
+import "./Home.css";
+
+const Home = () => {
+  //const storedUserId = sessionStorage.getItem('userId');
+  //console.log(storedUserId);
+  const balance = sessionStorage.getItem("amount");
+  const name = sessionStorage.getItem("Name");
+
+  return (
+    <Grid container spacing={8} className="pharma-container">
+      <Grid item lg={5} className="content">
+        <div className="bank-details-container">
+          <div className="text-container">
+            <h1 className="message-text">Welcome back, {name}!</h1>
+            <div className="balance-container">
+              <p className="balance">${balance}</p>
+              <p className="balance-text">Current Balance</p>
+            </div>
+          </div>
+          <div className="button-container">
+            <Button component={Link} to="/signup" id="getHelpButton">
+              E-Transfer
+            </Button>
+            <Button component={Link} to="/signup" id="getHelpButton">
+              View Transaction History
+            </Button>
+            <Button component={Link} to="/signup" id="getHelpButton">
+              Get Help Today
+            </Button>
+          </div>
+        </div>
+      </Grid>
+      <Grid item lg={7} className="image-container">
+        <Paper elevation={1} className="image" />
+      </Grid>
+    </Grid>
+  );
+};
+
+export default Home;
